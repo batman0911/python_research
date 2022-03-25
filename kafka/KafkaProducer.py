8242888accc60b7f1c41e0f4177f6b20e3347987
linhnm33 <linhnm33@ghtk.co>
2022-03-25 13:42:28 +0700

    kafka producer test

--- kafka/KafkaProducer.py
+++ kafka/KafkaProducer.py
@@ -1,8 +1,28 @@
 from confluent_kafka import Producer
 
+from common.Singleton import Singleton
 
-class KafkaProducer:
-    __producer = Producer({'bootstrap.server': 'localhost:9092'})
 
-    def __init__(self):
-        pass
+class KafkaProducer(metaclass=Singleton):
+    __producer = Producer({'bootstrap.servers': 'localhost:9092'})
+
+    def producer(self):
+        return self.__producer
+
+
+def delivery_report(err, msg):
+    """ Called once for each message produced to indicate delivery result.
+        Triggered by poll() or flush(). """
+    if err is not None:
+        print('Message delivery failed: {err}')
+    else:
+        print(f'Message delivered to {msg.topic()} [{msg.partition()}]')
+
+
+if __name__ == '__main__':
+    kafka_producer = KafkaProducer()
+    for i in range(10):
+        msg = f"message {i}"
+        kafka_producer.producer().produce("python-kafka", key="my_name", value=msg, callback=delivery_report)
+
+    kafka_producer.producer().flush()
